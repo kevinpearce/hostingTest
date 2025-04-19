d597c197e8f28c21d5507e765cfab456285c90c2
Kevin Pearce <kevinjohnpearce@googlemail.com>
2025-04-19 19:24:29 +0100

    hardcode baseurl

--- HostingTest/Program.cs
+++ HostingTest/Program.cs
@@ -4,13 +4,9 @@ using HostingTest;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-var baseAddress = builder.HostEnvironment.IsProduction()
-    ? "hostingTest/"
-    : builder.HostEnvironment.BaseAddress;
-
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
