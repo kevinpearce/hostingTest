1c02a808d43378cf4f090ac2cd841cac77fd25fc
Kevin Pearce <kevinjohnpearce@googlemail.com>
2025-04-20 20:32:32 +0100

    add icon component test

--- HostingTest/Program.cs
+++ HostingTest/Program.cs
@@ -4,9 +4,11 @@ using HostingTest;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
