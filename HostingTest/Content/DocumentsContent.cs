7669c3a55a7f90aea56e198671188ee91090cdcc
Kevin Pearce <kevinjohnpearce@googlemail.com>
2025-04-22 19:27:58 +0100

    combine static content - httpclient latency was annoying

--- HostingTest/Content/DocumentsContent.cs
+++
@@ -1,7 +0,0 @@
-namespace HostingTest.Content;
-
-public static class DocumentsContent
-{
-    public const string DocumentsParagraph =
-        "Please find below all documents/forms related to our organization.";
-}
---
+++ HostingTest/StaticContent.cs
@@ -0,0 +1,27 @@
+namespace HostingTest;
+
+public static class StaticContent
+{
+    public const string About =
+        """
+        We were founded in xxxxxxxx.
+        We believe in xxxxxxx.
+        Our mission is to xxxxxxx.
+        """;
+    
+    public const string Contact =
+        """
+        Although the organisation maintains a Social Media presence, we do not use it for official communication.
+        Please find below all contacts related to positions within our commitee.
+        """;
+    
+    public const string Schedule =
+        """
+        We meet every Tuesday at 6:00 PM in the main conference room.
+        The meeting is open to all members and interested parties.
+        Other meetings may be scheduled as needed - see Social Media for updates.
+        """;
+    
+    public const string Documents =
+        "Please find below all documents/forms related to our organization.";
+}
