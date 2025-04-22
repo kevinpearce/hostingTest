637f676f3935296e1f0fac4b4e1e736de888c94a
Kevin Pearce <kevinjohnpearce@googlemail.com>
2025-04-22 19:21:51 +0100

    Revert "move all text content"
    This reverts commit 0f3e99a5b2772bdcec1e2d8347f710f3ba6fcbc3.

---
+++ HostingTest/Content/AboutContent.cs
@@ -0,0 +1,11 @@
+namespace HostingTest.Content;
+
+public static class AboutContent
+{
+    public const string AboutParagraph =
+        """
+        We were founded in xxxxxxxx.
+        We believe in xxxxxxx.
+        Our mission is to xxxxxxx.
+        """;
+}
---
+++ HostingTest/Content/DocumentsContent.cs
@@ -0,0 +1,7 @@
+namespace HostingTest.Content;
+
+public static class DocumentsContent
+{
+    public const string DocumentsParagraph =
+        "Please find below all documents/forms related to our organization.";
+}
