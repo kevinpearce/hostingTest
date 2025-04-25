c010719e61475737ac8b9b407596717fdb34dc70
Kevin Pearce <42847716+kevinpearce@users.noreply.github.com>
2025-04-25 15:08:11 +0100

    rename directories

---
+++ hostingTest/Models/Document.cs
@@ -0,0 +1,7 @@
+namespace hostingTest.Models;
+
+public class Document
+{
+    public required string Name { get; init; }
+    public required string Path { get; init; }
+}
---
+++ hostingTest/Models/Person.cs
@@ -0,0 +1,8 @@
+namespace hostingTest.Models;
+
+public class Person
+{
+    public required string Name { get; init; }
+    public required string Position { get; init; }
+    public required string Email { get; init; }
+}
