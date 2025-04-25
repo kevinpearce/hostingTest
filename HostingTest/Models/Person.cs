703cfee838955b6574aee5fe97a891edee4c0826
Kevin Pearce <42847716+kevinpearce@users.noreply.github.com>
2025-04-25 15:10:59 +0100

    Delete HostingTest directory

--- HostingTest/Models/Person.cs
+++
@@ -1,8 +0,0 @@
-namespace hostingTest.Models;
-
-public class Person
-{
-    public required string Name { get; init; }
-    public required string Position { get; init; }
-    public required string Email { get; init; }
-}
