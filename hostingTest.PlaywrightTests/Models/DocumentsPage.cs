8f1f3e2c54926799f36a9a430f3c2591496d3c53
Kevin Pearce <42847716+kevinpearce@users.noreply.github.com>
2025-06-06 17:01:03 +0100

    switch to root path in config

--- hostingTest.PlaywrightTests/Models/DocumentsPage.cs
+++ hostingTest.PlaywrightTests/Models/DocumentsPage.cs
@@ -5,13 +5,14 @@ namespace hostingTest.PlaywrightTests.Models;
 
 public class DocumentsPage(IPage page, IConfiguration config)
 {
-    private readonly string _pagePath =
-        config["TestSettings:DocumentsPagePath"] ?? throw new InvalidOperationException();
+    private const string SubPath = "/documents";
+    
+    private readonly string _pagePath = config["TestSettings:RootPagePath"] ?? throw new InvalidOperationException();
 
     public IPage Page => page;
     
     public async Task GoToPageAsync()
     {
-        await page.GotoAsync(_pagePath);
+        await page.GotoAsync($"{_pagePath}{SubPath}");
     }
 }
